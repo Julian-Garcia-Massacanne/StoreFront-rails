fdfac5718ef33276a3c02b02fd43701078ae0c44
Julian-Garcia <julian020404@gmail.com>
2024-12-02 18:21:12 -0300

    Se agrega la funcionalidad de crear remeras

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,4 +12,33 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Producto creado exitosamente"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :unit_price,
+      :stock,
+      :category,
+      :size,
+      :color,
+      images: []
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   root to: "products#index"
 
 
-  resources :products, only: [ :index, :show ]
+  resources :products, only: [ :index, :show, :new, :create ]
 end
